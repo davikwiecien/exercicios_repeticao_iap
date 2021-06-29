02539227147c5a48b797d759f128890122dcd389
davikwiecien <66526045+davikwiecien@users.noreply.github.com>
2021-06-29 12:38:37 -0300

    Update exercicio-8.c

--- exercicio-8.c
+++ exercicio-8.c
@@ -1,3 +1,10 @@
+/*
+Implemente em linguagem C algoritmo que leia uma quantidade desconhecida de
+números e conte quantos deles estão nos seguintes intervalos: [0-25], [26-50], [51-
+75] e [76- ∞ ]. A entrada de dados deve terminar quando for lido um número
+negativo.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
