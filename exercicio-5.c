cb8353bd6f67c81ad44025c5c0d549d8ae825e47
davikwiecien <66526045+davikwiecien@users.noreply.github.com>
2021-06-29 12:28:56 -0300

    Add files via upload

---
+++ exercicio-5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    int tamanho, cont = 0;
+
+    printf("\nInforme o tamanho do triangulo (de 1 a 100): ");
+    scanf("%d", &tamanho);
+
+    while ((tamanho < 1) || (tamanho > 100)) {
+
+        if (cont == 0)
+
+            printf("\n");
+
+        printf("Tamanho invalido! Digite novamente: ");
+        scanf("%d", &tamanho);
+
+        cont++;
+
+    }
+
+    printf("\n");
+
+    for (int i = 1; i <= tamanho; i++) {
+
+        for (int j = 1; j <= i; j++)
+
+            printf("#");
+
+        printf("\n");
+
+    }
+
+    printf("\n");
+
+    return 0;
+
+}
