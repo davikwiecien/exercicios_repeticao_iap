622251ec97bb29d7fc6da8c39e8fd7a6c85e646f
davikwiecien <66526045+davikwiecien@users.noreply.github.com>
2021-06-29 12:39:24 -0300

    Update exercicio-9-while.c

--- exercicio-9-while.c
+++ exercicio-9-while.c
@@ -1,3 +1,13 @@
+/*
+João foi desafiado por seu irmão, José. O desafio era calcular quantos grãos de
+arroz são possíveis colocar em um tabuleiro de xadrez de dimensão N x N (o
+usuário deve informar a dimensão do tabuleiro), de tal forma que o primeiro quadro
+contivesse apenas um grão e os quadros subsequentes, o dobro do quadro
+anterior. Implemente em linguagem C, algoritmo que exiba para o usuário a
+quantidade de grãos em cada quadro e a quantidade total de grãos no tabuleiro.
+Exemplo: Tabuleiro 2 x 2; Total de grãos é 15;
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
